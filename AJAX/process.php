3ef5c080db301e1c96bb71cfdcc61089f77dcb45
tanphat13 <phat.cu_bkhcm1312@hcmut.edu.vn>
2020-03-09 15:58:20 +0700

    Update Ajax & FetchAPI

--- AJAX/process.php
+++ AJAX/process.php
@@ -2,4 +2,25 @@
 
 echo 'Processing...';
 
+// Connect to a database
+$conn = mysqli_connect('localhost', 'root', 'Tanphat123', 'ajaxtest');
+
+// Check for POST variable
+if(isset($_POST['name'])) {
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    echo 'GET: Your name is '. $_POST['name'];
+
+    $query = "INSERT INTO users(name) VALUES('$name')";
+    
+    if(mysqli_query($conn, $query)) {
+        echo 'User Added...';
+    } else {
+        echo 'ERROR: '. mysqli_error($conn);
+    }
+}
+
+// Check for GET variable
+if(isset($_GET['name'])) {
+    echo 'GET: Your name is '. $_GET['name'];
+}
 ?>
