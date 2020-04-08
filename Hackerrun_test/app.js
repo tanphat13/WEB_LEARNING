4b61be360c2a0afcc82c41524615710e2c95f22e
tanphat13 <phat.cu_bkhcm1312@hcmut.edu.vn>
2020-04-08 23:51:41 +0700

    I do something idk

---
+++ Hackerrun_test/app.js
@@ -0,0 +1,23 @@
+function hourglassSum(arr) {
+  let sum = 0;
+  let n = 2;
+  for (let i = 0; i < n; i++) {
+    let S = [repeat[]]
+  }
+  return sum;
+}
+
+var arr = [
+  [1, 1, 1, 0, 0, 0],
+  [2, 1, 3, 0, 0, 0],
+  [1, 1, 1, 0, 0, 0],
+  [0, 0, 2, 4, 4, 0],
+  [0, 0, 0, 2, 0, 0],
+  [0, 0, 1, 2, 4, 0]
+];
+
+const result = hourglassSum(arr);
+console.log(result);
+
+
+let name = document.getElementsByTagName('div');
